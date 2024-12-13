4a9b660b73c340121a5e216bb616ba0f5d481e5e
Cesar Hernandez <fernando.hernandez1001@outlook.com>
2024-12-13 15:22:47 -0600

    +nicoPages

---
+++ verProducto.js
@@ -0,0 +1,143 @@
+const firstButton = document.getElementById("firstButton")
+
+firstButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class");
+    const x = document.getElementById("universal-x")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const secondButton = document.getElementById("secondButton")
+
+secondButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class2");
+    const x = document.getElementById("universal-x2")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const tercerButton = document.getElementById("tercerButton")
+
+tercerButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class3");
+    const x = document.getElementById("universal-x3")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const cuartoButton = document.getElementById("cuartoButton")
+
+cuartoButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class4");
+    const x = document.getElementById("universal-x4")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const quintoButton = document.getElementById("quintoButton")
+
+quintoButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class5");
+    const x = document.getElementById("universal-x5")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const sextoButton = document.getElementById("sextoButton")
+
+sextoButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class6");
+    const x = document.getElementById("universal-x6")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const septimoButton = document.getElementById("septimoButton")
+
+septimoButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class7");
+    const x = document.getElementById("universal-x7")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
+
+const octavoButton = document.getElementById("octavoButton")
+
+octavoButton.addEventListener("click", ()=> {
+    const div = document.getElementById("div-class8");
+    const x = document.getElementById("universal-x8")
+    if (div.classList.contains("container-ver-prod-hidden")) {
+        div.classList.remove("container-ver-prod-hidden")
+        div.classList.add("container-ver-prod")
+    }
+
+    x.addEventListener("click", ()=> {
+        if (div.classList.contains("container-ver-prod")) {
+            div.classList.remove("container-ver-prod")
+            div.classList.add("container-ver-prod-hidden")
+        }
+    })
+})
